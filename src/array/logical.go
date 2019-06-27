43b8bff90747cd0d3f9438ff47be53cf000847f0
willhyper <willhyper@gmail.com>
2019-06-26 18:20:53 -0700

    add func DotSrc(src []bool, i1, i2[]int) int

--- src/array/logical.go
+++ src/array/logical.go
@@ -12,6 +12,18 @@ func Dot(a, b []bool) int{
 	return sum
 }
 
+func DotSrc(src []bool, indices1, indices2[]int) int{
+	N := len(indices1)
+	sum:=0
+	for i:=0;i<N;i++{
+		tf := src[indices1[i]] && src[indices2[i]]
+		if tf{
+			sum+=1
+		}
+	}
+	return sum
+}
+
 func GetValues(src []bool, indices []int) *[]bool{
 	v := make([]bool, len(indices))
 	for i:=0;i<len(indices);i++{
