f3a3abb78d5e7e35499505f56ce0b0d02a4f6244
willhyper <willhyper@gmail.com>
2019-07-04 23:00:03 -0700

    add func (m *Matrix) String() string

--- src/mat/struct.go
+++ src/mat/struct.go
@@ -1,6 +1,20 @@
 package mat
 
+import (
+	"array"
+	"fmt"
+)
+
 type Matrix struct {
 	arr  []bool
 	R, C int
+}
+
+func (m *Matrix) String() string {
+	s := ""
+	for r := 0; r < m.R; r++ {
+		Mr := *array.ToInt(m.Row(r))
+		s += fmt.Sprintf("%v\n", Mr)
+	}
+	return s
 }
