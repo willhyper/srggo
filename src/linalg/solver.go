17b4891fa49710ac03d8da62069468da20439a32
willhyper <willhyper@gmail.com>
2019-07-05 17:25:43 -0700

    rm src/linalg/betterSolver.go

--- src/linalg/solver.go
+++ src/linalg/solver.go
@@ -1,5 +1,8 @@
 package linalg
 
+import (
+	"fmt"
+)
 
 func Solve(q *Question) (<-chan *Answer){
 
@@ -9,4 +12,17 @@ func Solve(q *Question) (<-chan *Answer){
 	go betterSolver(q, ch)
 	
 	return ch
+}
+
+
+func betterSolver(q *Question, ch chan *Answer){
+	defer close(ch)
+
+	fmt.Println(q)
+	q.CompressCols()
+	fmt.Println(q)
+	// under construction
+	
+	ch <- NewAnswerDefault(3)
+
 }
