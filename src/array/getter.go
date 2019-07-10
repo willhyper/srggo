4cf802c4c05ee6413ee5ff8ec38002278bb9e45e
willhyper <willhyper@gmail.com>
2019-07-10 11:17:21 -0700

    add func SingleOutElement([]int, int) ([]int, int)

--- src/array/getter.go
+++ src/array/getter.go
@@ -1,10 +1,22 @@
 package array
 
-
-func GetValues(src []bool, indices []int) *[]bool{
+func GetValues(src []bool, indices []int) *[]bool {
 	v := make([]bool, len(indices))
-	for i:=0;i<len(indices);i++{
+	for i := 0; i < len(indices); i++ {
 		v[i] = src[indices[i]]
 	}
 	return &v
+}
+
+func SingleOutElement(a []int, index int) ([]int, int) {
+	b := make([]int, len(a)-1)
+
+	for i := 0; i < index; i++ {
+		b[i] = a[i]
+	}
+	v := a[index]
+	for i := index + 1; i < len(a); i++ {
+		b[i-1] = a[i]
+	}
+	return b, v
 }
